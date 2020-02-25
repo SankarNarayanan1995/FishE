b3e389232c6546b07bbb2c0e762caff28cd95222
SankarNarayanan1995 <sansat95@gmail.com>
2020-02-25 23:30:44 +0400

    CORS added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express'); // returns a function
 const bodyParser = require('body-parser');
 const app = express();
+const cors  = require('cors');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
+app.use(cors());
 const dbURL = "mongodb+srv://admin:Sn95Nryn@cluster0-jkaby.mongodb.net/test?retryWrites=true&w=majority";
 const mongoose =  require('mongoose');
 const regularRoutes =  require("./routes/Regular");
